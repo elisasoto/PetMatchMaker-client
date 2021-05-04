c292d8ba93f71e425c3fcf8fa16c27d96c0b578e
elisasoto <71519081+elisasoto@users.noreply.github.com>
2021-05-04 11:45:32 +0200

    appinit page created

---
+++ pet-match-maker/src/pages/AppInit.jsx
@@ -0,0 +1,10 @@
+import Access from '../components/AppInit/Access';
+
+export default function AppInit() {
+  return (
+    <div>
+      <h1>Welcome to PetMatchMaker</h1>
+      <Access />
+    </div>
+  );
+}
