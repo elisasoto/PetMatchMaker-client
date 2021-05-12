25468610d045f58f34390c35ef7ff4095bcfcfa7
elisasoto <71519081+elisasoto@users.noreply.github.com>
2021-05-12 20:33:12 +0200

    watch function for text area standarized for shelter, pet and user forms

--- pet-match-maker/src/components/Forms/ShelterRegisterForm/ShelterRegister.jsx
+++ pet-match-maker/src/components/Forms/ShelterRegisterForm/ShelterRegister.jsx
@@ -32,13 +32,10 @@ export default function ShelterRegister() {
 
   const handleClick = () => setShow(!show);
 
-  const textAreaRecalculate = (e) => {
-    settextAreaLengthCount(e.target.value.length);
-  };
-
   const {
     handleSubmit,
     register,
+    watch,
     formState: { errors }
   } = useForm({});
 
@@ -46,11 +43,10 @@ export default function ShelterRegister() {
     console.log(formValues);
   };
 
+  const [about] = watch(['about']);
+
   return (
-    <form
-      onSubmit={handleSubmit(handleFormSubmit)}
-      onChange={textAreaRecalculate}
-    >
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
       <Stack spacing={2}>
         <FormControl isRequired={errors.name} isInvalid={!!errors.name}>
           <InputGroup size="sm">
@@ -244,7 +240,9 @@ export default function ShelterRegister() {
                 minLength: 50
               })}
             />
-            <p> {`${aboutCounthLength} / 240 left`} </p>
+            <p>
+              <strong>{`${about.length || 0} / 240 left`}</strong>{' '}
+            </p>
           </Box>
 
           <FormErrorMessage>
